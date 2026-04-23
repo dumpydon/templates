9d121d388b01c251d004696ad10d4ee926bfc638
dumpydon <apiyush171@gmail.com>
2026-04-24 05:14:33 +0530

    Create PrimeSieveEratosthenes.py
    PRIME SIEVE TEMPLATE

---
+++ PrimeSieveEratosthenes.py
@@ -0,0 +1,21 @@
+
+#------------------------------------------------------------------------------
+#PRIME SIEVE TEMPLATE
+
+        N= 5*10**6      #❇️    #N is the range till you wat sieve to be 
+
+        prime = [True]*(N+1) #❇️ 
+        #-----------------------------------------------
+        p = 2 #starting with the first prrime.   
+        while p*p<=N:
+
+            if prime[p]==True:
+                for i in range(p*p , N+1, p): #❇️ 
+                    prime[i]==False
+            p+=1
+
+        #printing prime number 
+        #-----------------------------------------------
+        # ls =  [i for i in range(2, N+1) if prime[i]==True]   #❇️ 
+#------------------------------------------------------------------------------
+
