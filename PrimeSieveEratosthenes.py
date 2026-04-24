188ef4d52700e66856f917ca528039654c6ead12
dumpydon <apiyush171@gmail.com>
2026-04-24 05:34:35 +0530

    Prime 🪜 SIEVE Template complete

--- PrimeSieveEratosthenes.py
+++ PrimeSieveEratosthenes.py
@@ -1,21 +1,43 @@
-
+# Prime 🪜 SIEVE Template
 #-----------------------------------------------------------------------------
 #PRIME SIEVE TEMPLATE
         #N is the range till you want sieve to be 
-        N= 5*10**6      #❇️ 5 * 10**6 is very big    cant go 10**7 in python  
+def sieve(N):
+        
+    N = 5* 10**16     #or  max(arr 
+    prime = [True]*(N+1)
+    prime[0] = prime[1] = False
+
+    for p in range(2, int(N**0.5) + 1):
+
+        if prime[p]:
+            for i in range(p*p, N+1, p):
+                prime[i] = False
+        
+    return prime
+
+#Time Complexity: O(N log log N)
+#Space Complexity: O(N)
+  
+#-----------------------------------------------------------------------------
+#PRIME SIEVE SLOWER TEMPLATE
+        #N is the range till you want sieve to be 
+        # N= 5*10**6*2   #❇️    #to be on safer side of length of sieve
+        N = max(temp)
 
         prime = [True]*(N+1) #❇️ 
-        #-----------------------------------------------
+        prime[0] = prime[1] = False
+
         p = 2 #starting with the first prrime.   
+
         while p*p<=N:
 
             if prime[p]==True:
                 for i in range(p*p , N+1, p): #❇️ 
-                    prime[i]==False
+                    prime[i]=False
             p+=1
 
         #printing prime number 
-        #-----------------------------------------------
-        # ls =  [i for i in range(2, N+1) if prime[i]==True]   #❇️ 
+        # ls =  [i for i in range(2, N+1) if prime[i]==True] #❇️ 
 #------------------------------------------------------------------------------
 
