2f4ea6f00bd83c347f09535a5668995eb09f92fd
dumpydon <apiyush171@gmail.com>
2026-05-22 08:38:40 +0530

    🦌Difference🦌Array🦌Techniqye

---
+++ DIFFERENceARRAY.py
@@ -0,0 +1,42 @@
+#---------------------------
+    #difference array technique template 
+    diff = [0]*(n)
+    
+    # x = 1
+    for u, v, x in queries:
+        
+        diff[u] += x
+        if v+1 <n:
+            diff[v+1] -= x
+    
+    #-----------------------------------
+    #CUMMULATING DIFFERENCE ARRAY WE MADE
+    res= []
+    curr = 0 
+    for delta in diff:
+        curr += delta
+        res.append(curr)
+    # print(res) #= [0, 1, 2]
+    #-----------------------------------
+
+
+
+
+
+        
+        
+    #BELOW THIS USE THE ABOVE DIFFERNCE ARRAY CODE AND WRITE SOLUTION
+    #-----------------------------------------------------
+    #2381. Shifting Letters II
+    #-----------------------------------------------------
+    # #CREATING NEW CH
+    # ans = []
+    # for i in range(n):
+    #     ch = s[i]
+    #     neword =  ( ord(ch)-97 + res[i] )%26  +97
+
+    #     newch = chr(neword)
+    #     ans.append(newch)
+    
+    # return "".join(ans)
+    #-----------------------------------------------------
