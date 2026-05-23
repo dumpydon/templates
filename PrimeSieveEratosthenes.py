8d89149618e96d48593a4e5d53003dab57b03dde
dumpydon <apiyush171@gmail.com>
2026-05-23 17:56:20 +0530

    # 🎃PRIME🎃SIEVE🎃TEMPLATE🎃

--- PrimeSieveEratosthenes.py
+++ PrimeSieveEratosthenes.py
@@ -32,6 +32,7 @@
 #WHEN n <= 1000
 #--------------------------------------------------
 def isprime(n):
+    if n < 2: return False
     for d in range(2, int(n**0.5)+1):
         if n%d==0:
             return False
