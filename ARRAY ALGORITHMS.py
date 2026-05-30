c664fa59afc44a59805c7c7f1ab023e87b4d8db5
dumpydon <apiyush171@gmail.com>
2026-05-31 00:00:00 +0530

    DUTCH NATIONAL FLAG ADDED

---
+++ ARRAY ALGORITHMS.py
@@ -0,0 +1,25 @@
+#ARRAYS ALGORITHMS
+
+
+
+#===============================================================================================================
+#1 DUTCH NATIONAL FLAG  ALGORITHM
+#===============================================================================================================
+        #this is a dutch national flag thing
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:  # nums[mid] == 2
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+#===============================================================================================================
