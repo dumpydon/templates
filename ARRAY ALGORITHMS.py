798ab47c28f53d168e808f1789f667048b936b07
dumpydon <apiyush171@gmail.com>
2026-08-13 07:47:00 +0530

    Add Dutch National Flag algorithm for sorting
    Implemented Dutch National Flag algorithm to sort colors in the array.

--- ARRAY ALGORITHMS.py
+++ ARRAY ALGORITHMS.py
@@ -16,6 +16,29 @@ nums.sort()
 mia = nums[0]
 mib = nums[1]
 
+#======================================================================================
+# DUTCH NATIONAL FLAG ALGORITHM  🟥 ⬜️ 🟦
+#======================================================================================
+
+        zeros = 0
+        ones = 0
+        twos = len(nums) - 1
+
+        while ones <= twos:
+
+            if nums[ones] == 0:
+                nums[zeros], nums[ones] = nums[ones], nums[zeros]
+                zeros += 1
+                ones += 1
+
+            elif nums[ones] == 1:
+                ones += 1
+
+            else:   # nums[ones] == 2
+                nums[ones], nums[twos] = nums[twos], nums[ones]
+                twos -= 1
+
+#======================================================================================
 
 #======================================================================================
 #1 DUTCH NATIONAL FLAG  ALGORITHM  🟥 ⬜️ 🟦
