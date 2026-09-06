7fc48fcea6f4ef7133fabe72b0d20cd8e2a241f8
dumpydon <apiyush171@gmail.com>
2026-09-07 01:52:48 +0530

    Enhance difference array implementation and examples
    Refactor and document the difference array technique for range updates.

--- DIFFERENceARRAY.py
+++ DIFFERENceARRAY.py
@@ -1,31 +1,89 @@
-#---------------------------
-    #difference array technique template 
-    diff = [0]*(n)
+# Difference array is applied when we have to repeatedly do the range addition things. 
+ # And we have to query fewer times. 
+
+
+#DIFFERENCE ARRAY TECHNIQUE-----------------------------------------
+    #--------------------------------------------------------
+    n = len(arr)
     
-    # x = 1
-    for u, v, x in queries:
+    temp  = [0]*(n+1) #or n or n+5 whatever
+    
+    for a,b, x in queries:
+        temp[a]   +=x
+        temp[b+1] -=x
+    
+    #--------------------------------------------------------
+    #ACCUMULATION STEP 
+    arr = list(accumulate(temp))
+ 
+    diff = list(accumulate(temp))
+    
+    #PREFIX SUM WAY
+    #----------------------------------------
+    # for i in range(1,n)
+    #     temp[i]+=temp[i-1]
+    # arr = temp[:]
         
-        diff[u] += x
-        if v+1 <n:
-            diff[v+1] -= x
-    
-    #-----------------------------------
-    #CUMMULATING DIFFERENCE ARRAY WE MADE
-
-    for i in range(1,n):
-        diff[i] += diff[i-1]
-    # print(diff) #= [0, 1, 2]
-    #-----------------------------------
+    #------------------------------------
+    # arr = []
+    # curr=0
+    # for el in temp:
+    #     curr+=el
+    #     arr.append(curr)
+    #--------------------------------------------------
 
 
 
-    # res= []
-    # curr = 0 
-    # for delta in diff:
-    #     curr += delta
-    #     res.append(curr)
-    # print(res) #= [0, 1, 2]
-    #-----------------------------------
+
+
+
+
+
+
+"""
+# For example,
+#---------------------------------------------------------------------------------------------------------
+arr = [2, 9, 4, 5, 21, 4] 
+       0  1  2  3   4  5 
+
+querry = [[1,2,+1],  [0,4,-1],  [3,5,+3]]
+           a b  x     a b x      a b x
+
+arr = [2,  9,  4,  5,  21,  4] 
+          +1  +1
+      -1  -1  -1  -1  -1 
+                  +3  +3   +3 
+#--------------------------------------
+diff =[-1  0  0  +2   +2   +3]
+#--------------------------------------
+newarr=[1, 9, 4,  7,   23,   7]
+#--------------------------------------
+
+
+to do this we can do like 
+#DIFFERENCE ARRAY TECHNIQUE--------------------------------------------
+       
+arr = [2,  9,  4,  5,  21,  4] 
+
+temp = [0, 0,  0,  0,  0,   0,  0 ]
+convertit to
+       [-1 +1      +2       +1   -2  #THIS IS WHAT DIFFERENCE ARRAY DO
+accumulate
+diff   [-1  0  0   +2  +2   +3   -1]
+
+diff = list(accumulate(temp)))
+#--------------------------------------
+diff =[-1  0  0  +2   +2   +3]
+#--------------------------------------
+#---------------------------------------------------------------------------------------------------------
+"""
+ 
+
+
+
+
+
+
 
 
 
